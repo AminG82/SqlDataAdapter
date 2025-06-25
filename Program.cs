c3395b1afc038caa0579dd85cdb175adce04b4b7
Amin Ghasemi <mmnghasemi@gmail.com>
2025-06-25 14:03:52 +0330

    initialCommit And Testing DataBase / Connection string Fixed

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿
 using Microsoft.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 SqlConnection connection = new SqlConnection("""
-    Data Source=.;Initial Catalog=TestDB;User ID=sa;Encrypt=FalseData Source=.;
-    Initial Catalog=TestDB;User ID=sa;Encrypt=False
+    Data Source =.; Initial Catalog = TestDB; User ID = sa; Password = amin5123; Encrypt = False;
     """);
 
 
@@ -13,6 +13,7 @@ SqlCommand command = new SqlCommand("""
 
 connection.Open();
 int count = (int)command.ExecuteScalar();
+Console.WriteLine($"Number of records in Person table: {count}");
 connection.Close();
 
 
