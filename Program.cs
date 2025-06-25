c9a8b85f0ddfc3e83bc15b6096852a387195b415
Amin Ghasemi <mmnghasemi@gmail.com>
2025-06-25 13:58:25 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("""
+    Data Source=.;Initial Catalog=TestDB;User ID=sa;Encrypt=FalseData Source=.;
+    Initial Catalog=TestDB;User ID=sa;Encrypt=False
+    """);
+
+
+SqlCommand command = new SqlCommand("""
+    SELECT COUNT(*) FROM Person
+    """, connection);
+
+connection.Open();
+int count = (int)command.ExecuteScalar();
+connection.Close();
+
+
